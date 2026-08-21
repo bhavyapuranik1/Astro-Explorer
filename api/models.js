2fef206c8ed6f1979e99a316e777cbf86b972170
bhavyapuranik1 <bhavyapuranik2006@gmail.com>
2026-08-21 20:28:46 +0530

    fix(api): remove unused import in api/models.js for Vercel serverless function compatibility

--- api/models.js
+++ api/models.js
@@ -1,6 +1,5 @@
-import { getModelCapability } from "../model-capabilities.js";
-
 const ALLOWED_LOCAL_ORIGINS = [
+
   "http://127.0.0.1:5501",
   "http://localhost:5500",
   "http://localhost:5501",
